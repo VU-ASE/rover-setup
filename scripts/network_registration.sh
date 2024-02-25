c603ce54c8acbc21c3fdce2938a695d98fec0d60
maxgallup <maxgallup@pm.me>
2024-02-25 17:39:59 +0100

    chore: made code dry

--- scripts/network_registration.sh
+++ scripts/network_registration.sh
@@ -18,8 +18,11 @@ run() {
 	$1 || error "failed command: $1"
 }
 
+CUSTOM_HOSTNAME=""
+
 set_hostname() {
 	debug "setting hostname to $1"
+	CUSTOM_HOSTNAME="$1"
 	# run "sudo hostnamectl set-hostname $1"
 }
 
@@ -32,26 +35,25 @@ if [ -z "$WIFI_MAC" ] && [ -z "$ETHER_MAC" ]; then
 	error "Could not find mac address of devices: wlan0 and ens33"
 fi
 
+CUSTOM_NAME_PATH="/home/debix/rover-setup/network/custom_hostname"
+CURRENT_HOST_NAME=$(cat $CUSTOM_NAME_PATH | xargs)
 
-CURRENT_HOST_NAME=$(cat /home/debix/rover-setup/network/custom_hostname)
 RANDOM_NAME=$(wget -q -O - https://random-word-api.herokuapp.com/word?length=5 | cut -c 3-7)
 
 if [ -z "$CURRENT_HOST_NAME" ]; then
-
 	if [ -n "$RANDOM_NAME" ]; then
 		set_hostname $RANDOM_NAME
 	else
 		set_hostname debix
 	fi
 
+	echo "$CUSTOM_HOSTNAME" > $CUSTOM_NAME_PATH
+
 else
 	set_hostname $CURRENT_HOSTNAME
 fi
 
 
-sleep 1
 
-HOST_NAME=$(hostname)
-
-success "$WIFI_MAC, $ETHR_MAC, $HOST_NAME"
+success "$WIFI_MAC, $ETHR_MAC, $CUSTOM_HOSTNAME"
 
