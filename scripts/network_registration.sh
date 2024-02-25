dcc3bd87ebacf8a216799186320a527e43263acf
maxgallup <maxgallup@pm.me>
2024-02-25 19:22:03 +0100

    fix: typo

--- scripts/network_registration.sh
+++ scripts/network_registration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# MAC_DB="/home/debix/rover-setup/network/mac_addresses"
+# MAC_DB="$REPO_DIR/network/mac_addresses"
 
 debug() {
 	echo "🟡 $1"
@@ -20,6 +20,8 @@ run() {
 
 CUSTOM_HOSTNAME=""
 
+REPO_DIR="/home/debix/rover-setup/network"
+
 set_hostname() {
 	debug "setting hostname to $1"
 	CUSTOM_HOSTNAME="$1"
@@ -33,7 +35,7 @@ if [ -z "$WIFI_MAC" ] && [ -z "$ETHER_MAC" ]; then
 	error "Could not find mac address of devices: wlan0 and ens33"
 fi
 
-CUSTOM_NAME_PATH="/home/debix/rover-setup/network/custom_hostname"
+CUSTOM_NAME_PATH="$REPO_DIR/custom_hostname"
 CUSTOM_HOSTNAME=$(cat $CUSTOM_NAME_PATH | xargs)
 
 RANDOM_NAME=$(wget -q -O - https://random-word-api.herokuapp.com/word?length=5 | cut -c 3-7)
@@ -53,9 +55,9 @@ fi
 
 DB_STRING="$WIFI_MAC, $ETHR_MAC, $CUSTOM_HOSTNAME"
 
-if [ -z "$(cat /home/debix/rover-setup/netowrk/mac_db | grep "$DB_STRING")" ]; then
+if [ -z "$(cat $REPO_DIR/mac_db | grep "$DB_STRING")" ]; then
 	debug "adding '$DB_STRING' to network/mac_db"
-	echo $DB_STRING >> /home/debix/rover-setup/network/mac_db
+	echo $DB_STRING >> $REPO_DIR/mac_db
 else
 	debug "$DB_STRING already in network/mac_db"
 fi
